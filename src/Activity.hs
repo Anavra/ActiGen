56c9fa00b1e50b50bebde8a868cfd4b15ec262a9
Ludvig Böklin <ludvig.boklin@protonmail.com>
2017-04-12 22:28:44 +0200

    Randomly list a given number of predefined activities

--- src/Activity.hs
+++ src/Activity.hs
@@ -1,5 +1,30 @@
 module Activity where
 
-type Tag = String
+import System.Random
 
-newtype Activity = Activity [Tag]
+import Test.QuickCheck
+
+type Tag = String
+type Description = String
+
+activities = [ (Activity "Draw"            [ "Artistic","Relaxing"])  :: Activity
+             , (Activity "Play the piano"  [ "Artistic", "Relaxing"]) :: Activity
+             , (Activity "Game"            [ "Mind", "Exciting"])     :: Activity
+             ]
+
+data Activity = Activity Description [Tag] deriving (Show)
+
+randomActivity :: [Activity] -> StdGen -> Activity
+randomActivity acts stdGen = (acts !!) . fst $ randomR (0, length activities - 1) stdGen
+
+randomActivities :: [Activity] -> Int -> StdGen -> [Activity]
+randomActivities acs c g
+    | c <= 0    = error $ "Unable to make a list of " ++ (show c) ++ " activities."
+    | c == 1    = [randomActivity acs g]
+    | otherwise = (rAc:rAcs)
+          where rAc  = randomActivity acs g
+                rAcs = randomActivities acs (c - 1) nG
+                nG = snd $ next g
+
+-- prop_randomActivity :: Int -> Bool
+-- prop_randomActivity x = randomActivity activities x !=
--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,55 @@
 module Main where
 
+import           System.Console.GetOpt
+import           System.Random
+import           System.Environment (getArgs, lookupEnv)
+
+import           Data.Maybe         (fromMaybe)
+import           System.Directory   (createDirectoryIfMissing)
+
+import           Activity
+
 main :: IO ()
 main = do
-  putStrLn "hello world"
+    args <- getArgs
+    g    <- newStdGen
+    -- | Generate random activities and join them into a single string with newlines
+    let output | argc <= 0 = "Too few arguments given."
+               | argc == 1 = unlines $ map show aList
+               | argc == 2 = "2 arguments given. This does nothing right now."
+               -- | argc == 2 = do
+               --     case arg1 of
+               --     return $ addNew (args !! 1)
+               | argc >= 3 = "Too many arguments given."
+               | otherwise = "Something is not right with the arguments."
+             where
+               argc        = length args
+               aList       = randomActivities activities arg1 g
+               cArg        = read $ head args
+               cArgDefault = 0
+               arg1 | cArg >= 1 = cArg
+                    | otherwise = cArgDefault
+    putStrLn output
+
+data Flag = Verbose | Version | Help
+          | AddNew Activity
+          | Remove Activity
+            deriving (Show)
+
+options :: [OptDescr Flag]
+options = [ Option ['a'] ["add", "new"] (ReqArg addNew "") "Add a new activity"
+          ]
+
+fileName :: FilePath
+fileName = "activities.txt"
+
+addNew :: String -> Flag
+addNew acString = AddNew $ activities !! 0
+
+appendNew :: Activity -> IO ()
+appendNew ac = do
+    dataDir <- lookupEnv "XDG_DATA_DIR"
+    let saveDir = fromMaybe "$HOME/.local/share/" dataDir ++ "actigen/" :: FilePath
+    let filePath = saveDir ++ fileName
+    createDirectoryIfMissing False saveDir
+    appendFile filePath (show ac)
