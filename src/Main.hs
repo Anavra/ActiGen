1755c8b7df65931da409a8f05f3c333dce736c45
Ludvig Böklin <ludvig.boklin@protonmail.com>
2017-04-15 12:05:14 +0200

    Update/Fix cabal file and refactor Main.hs

--- src/Main.hs
+++ src/Main.hs
@@ -1,10 +1,8 @@
 module Main where
 
--- import           System.Console.GetOpt
-import           System.Random
--- import           Text.Read
 import           Data.Maybe
 import           System.Environment (getArgs, lookupEnv)
+import           System.Random
 
 import           System.Directory   (createDirectoryIfMissing)
 
@@ -15,21 +13,18 @@ main = do
     args <- getArgs
     g    <- newStdGen
     -- | Generate random activities and join them into a single string with newlines
-    -- let n = read $ fromMaybe "1" $ listToMaybe args :: Int
+    let n = read $ fromMaybe "1" $ listToMaybe args :: Int
     let output
           | argc == 0 = "No arguments given, assuming "
-                        ++ show numberArg
+                        ++ show n
                         ++ " activity.\n"
-                        ++ getAcList
-          | argc == 1 = getAcList
+                        ++ mkActiList n g
+          | argc == 1 = mkActiList n g
           | argc == 2 = "2 arguments given. This does nothing right now."
           | argc >= 3 = "Too many arguments given."
           | otherwise = "Something is not right with the arguments."
-            where
+          where
             argc              = length args
-            getAcList         = unlines $ map show $ acList numberArg
-            acList n          = randomActivities n activities g
-            numberArg         = read $ fromMaybe "1" $ listToMaybe args :: Int
     putStrLn output
 
 data Flag = Verbose | Version | Help
@@ -37,22 +32,17 @@ data Flag = Verbose | Version | Help
           | Remove Activity
             deriving (Show)
 
-mkActiList :: Int -> IO ()
-mkActiList n = do
+mkActiList :: Int -> StdGen -> String
+mkActiList n g = unlines $ map show actis
+  where
+    actis = randomActivities n activities g
+
+newActiList :: Int -> IO String
+newActiList n = do
     g <- newStdGen
     let actis = randomActivities n activities g
     let actiList = unlines $ map show actis
-    putStrLn actiList
-
-
-
--- options :: [OptDescr Flag]
--- options = [ Option ['v'] ["verbose"] (NoArg Verbose) "Add a new activity"
---           , Option ['a'] ["add", "new"] (ReqArg addNew "") "Add a new activity"
---           ]
-
--- addNew :: String -> Flag
--- addNew acString = AddNew $ activities !! 0
+    return actiList
 
 fileName :: FilePath
 fileName = "activities.txt"
@@ -64,3 +54,11 @@ appendNew ac = do
     let filePath = saveDir ++ fileName
     createDirectoryIfMissing False saveDir
     appendFile filePath (show ac)
+
+-- addNew :: String -> Flag
+-- addNew acString = AddNew $ activities !! 0
+
+-- options :: [OptDescr Flag]
+-- options = [ Option ['v'] ["verbose"] (NoArg Verbose) "Add a new activity"
+--           , Option ['a'] ["add", "new"] (ReqArg addNew "") "Add a new activity"
+--           ]
